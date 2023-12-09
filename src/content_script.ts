8ac9bf4e291ac7bec17fbb024697991f498635d7
tomo0611 <tomo0611@hotmail.com>
2023-12-09 18:26:51 +0900

    課題の残り時間をホームに表示できるように

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,3 +1,4 @@
+// スクリプトが読み込まれた時の処理
 window.onload = function () {
     console.log("[Moodle Plus] content script script loaded");
     console.log("[Moodle Plus] UserStatus: logged in : " + isLoggedin());
@@ -5,6 +6,7 @@ window.onload = function () {
     if (window.location.pathname === "/" && isLoggedin()) {
         changeTitle();
         minimizeNewsFeed();
+        showUpcomingAsignments();
     }
 };
 
@@ -29,9 +31,13 @@ function changeTitle() {
         }
         if (title) {
             title.innerHTML = "む～どるぷらす (Moodle Plus)";
+            const subtitle = document.createElement("p");
+            subtitle.innerHTML = `Developed by tomo0611 (大阪公立大学 工学部 情報工)</br>
+            バグなどの報告は<a href="https://github.com/tomo0611/moodle-plus" target="_blank">こちら</a>までお願いします。`;
+            title.parentElement?.insertBefore(subtitle, title.nextElementSibling);
         }
     } catch (e) {
-        console.log("Title Format is not as expected");
+        console.log("[Moodle Plus] Title Format is not as expected");
         console.log(e);
     }
 }
@@ -47,7 +53,115 @@ function minimizeNewsFeed() {
             }
         }
     } catch (e) {
-        console.log("News Feed Format is not as expected");
+        console.log("[Moodle Plus] News Feed Format is not as expected");
         console.log(e);
     }
+}
+
+
+/**
+ * Dateオブジェクトを表示用の文字列に変換します。
+ *
+ * @param {Date} date Dateオブジェクト
+ * @param {boolean} [withDay=false] 日付を含めるかどうか
+ * @return {string} 表示用の文字列
+ */
+function dateToString(date: Date, withDay: boolean = false): string {
+    let h: number | string = date.getHours(); // 0 - 23
+    let m: number | string = date.getMinutes(); // 0 - 59
+    let s: number | string = date.getSeconds(); // 0 - 59
+    const day = date.getDate() - 1;
+
+    // 0埋め処理
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = h + ":" + m + ":" + s;
+    if (withDay && day !== 0) {
+        time = day + "日 " + time;
+    }
+    return time;
+}
+
+// 提出期限(unixtime)をまとめた配列
+const lefttime_list: number[] = [];
+
+/**
+ * 残り時間を表示します。
+ */
+function showTime() {
+
+    // 現在時刻を表示
+    let date = new Date();
+    let time = dateToString(date, false);
+    const realtime_clock = document.getElementById("realtime_clock");
+    if (realtime_clock) {
+        realtime_clock.innerText = time;
+        realtime_clock.textContent = time;
+    }
+
+
+    for (let i = 0; i < lefttime_list.length; i++) {
+        const lefttime = lefttime_list[i];
+        const lefttime_span = document.getElementsByClassName("left_realtime_clock")[i] as HTMLSpanElement;
+        const lefttime_date = new Date(lefttime * 1000 - date.getTime() - 1000 * 3600 * 9 - 1000 * 3600 * 24 * 1); // 
+        let lefttime_time = dateToString(lefttime_date, true);
+        if (lefttime_date.getDate() - 1 === 0) {
+            lefttime_span.style.color = "red";
+            lefttime_span.style.fontWeight = "bold";
+            lefttime_span.style.fontSize = "1.8em";
+        }
+        lefttime_span.innerText = lefttime_time;
+        lefttime_span.textContent = lefttime_time;
+    }
+
+    // 1秒後にまた実行
+    setTimeout(showTime, 1000);
+}
+
+/** 期限の近い課題を表示 */
+async function showUpcomingAsignments() {
+    const newNode = document.createElement("span");
+    newNode.innerHTML = `<h3>☆そろそろ提出せなあかん課題</h3>`;
+    newNode.innerHTML += `<p>現在の時刻：<span id="realtime_clock"></span> (※注意:ズレがある場合があります)</p>`;
+    const upcoming_data = await (await fetch("/calendar/view.php?view=upcoming")).text();
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(upcoming_data, 'text/html');
+    const events = htmlDoc.getElementsByClassName('eventlist my-1')[0].getElementsByClassName("event mt-3");
+    const length = events.length > 4 ? 4 : events.length;
+    for (let i = 0; i < length; i++) {
+        const event = events[i];
+        const event_title = (event.getElementsByClassName('name d-inline-block')[0] as HTMLHeadingElement).innerText;
+
+        // 期間の開始を除外
+        if (event_title.indexOf("可能期間の開始") !== -1) continue;
+
+        const cardBody = event.getElementsByClassName('description card-body')[0];
+        const event_unixtime = parseInt(cardBody.getElementsByClassName('col-11')[0].getElementsByTagName("a")[0].href.split("&time=")[1]);
+        lefttime_list.push(event_unixtime);
+        const event_date = (cardBody.getElementsByClassName('col-11')[0] as HTMLAnchorElement).innerText;
+        let event_url = (event.getElementsByClassName('card-link')[0] as HTMLAnchorElement).href;
+        if (event_url.indexOf("&action=") !== -1) {
+            event_url = event_url.split("&action=")[0];
+        }
+        const event_course = (event.getElementsByClassName('col-11')[event.getElementsByClassName('col-11').length - 1] as HTMLAnchorElement).innerText;
+        newNode.innerHTML += `
+        <div class="card my-2">
+            <div class="card-body">
+                <h6 class="card-subtitle">${event_course}</h6>
+                <h5 class="card-title">${event_title}</h5>
+                <div style="display: flex; justify-content: space-between;">
+                
+                    <h6 class="card-subtitle mb-2 text-muted">${event_date}<br/>残り時間>> <span class="left_realtime_clock"></span></h6>
+                    <a href="${event_url}" class="btn btn-primary" style="height: fit-content;">課題を確認する</a>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    document.getElementById("maincontent")?.parentElement?.insertBefore(newNode, document.getElementById("maincontent"));
+
+    showTime();
+
 }

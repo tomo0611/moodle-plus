5b157aeff870266e58713f5e10a71a9a69eb4067
tomo0611 <tomo0611@hotmail.com>
2023-12-09 17:44:06 +0900

    タイトル変更とサイトニュース最小化

--- src/content_script.ts
+++ src/content_script.ts
@@ -3,6 +3,8 @@ window.onload = function () {
     console.log("[Moodle Plus] UserStatus: logged in : " + isLoggedin());
 
     if (window.location.pathname === "/" && isLoggedin()) {
+        changeTitle();
+        minimizeNewsFeed();
     }
 };
 
@@ -13,4 +15,39 @@ window.onload = function () {
  */
 function isLoggedin() {
     return document.getElementsByClassName("usermenu")[0].children[0].className.indexOf("login") === -1;
+}
+
+
+/** メインページのタイトルを変更 */
+function changeTitle() {
+    try {
+        let title;
+        if (document.getElementsByClassName("page-header-headings").length === 0) {
+            title = document.getElementById("page-header")?.getElementsByTagName("h2")[0];
+        } else {
+            title = document.getElementsByClassName("page-header-headings")[0].getElementsByTagName("h1")[0];
+        }
+        if (title) {
+            title.innerHTML = "む～どるぷらす (Moodle Plus)";
+        }
+    } catch (e) {
+        console.log("Title Format is not as expected");
+        console.log(e);
+    }
+}
+
+/** サイトニュースを最小化 */
+function minimizeNewsFeed() {
+    try {
+        const newsFeeds = document.getElementById("site-news-forum")?.getElementsByClassName('d-flex body-content-container');
+        if (newsFeeds) {
+            for (let i = 0; i < newsFeeds.length; i++) {
+                newsFeeds[i].innerHTML
+                    = newsFeeds[i].getElementsByClassName("link text-right")[0].outerHTML;
+            }
+        }
+    } catch (e) {
+        console.log("News Feed Format is not as expected");
+        console.log(e);
+    }
 }

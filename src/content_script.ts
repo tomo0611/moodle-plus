24af3475dd517686c4d6207a4aae190b7cab399f
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-06-09 01:35:40 +0900

    fix indent

--- src/content_script.ts
+++ src/content_script.ts
@@ -49,54 +49,53 @@
         }
     }
 
-/** サイトニュースを最小化 */
-function minimizeNewsFeed() {
-    try {
-        const newsForumEl = document.getElementById("site-news-forum");
+    /** サイトニュースを最小化 */
+    function minimizeNewsFeed() {
+        try {
+            const newsForumEl = document.getElementById("site-news-forum");
 
-        if (!newsForumEl) {
-            console.log("[Moodle Plus] News Feed is not found");
-            return;
-        }
+            if (!newsForumEl) {
+                console.log("[Moodle Plus] News Feed is not found");
+                return;
+            }
 
-        const title = newsForumEl.getElementsByTagName("h2")[0];
-        if (title) {
-            title.innerText = "サイトニュース (コンパクト版)";
-        }
-        const lastlink = newsForumEl.lastElementChild?.lastElementChild as HTMLAnchorElement;
-        const subscribeButton = newsForumEl.getElementsByClassName("subscribelink")[0].children[0] as HTMLAnchorElement;
-        subscribeButton.innerText = "長いお知らせ達を読む";
-        subscribeButton.textContent = "長いお知らせ達を読む";
-        subscribeButton.href = lastlink.href;
-        const articles = newsForumEl.querySelectorAll("article.forum-post-container") as NodeListOf<HTMLDivElement>;
-        // create new element
-        const newNode = document.createElement("div");
-        newNode.innerHTML = `<h6>☆お知らせ (タイトルのみ)</h6>`;
-        if (articles.length > 0) {
-            const newArticles: string[] = [];
-            [...articles].forEach((article) => {
-                const title = (article.getElementsByClassName("h6 font-weight-bold mb-0")[0] as HTMLHeadingElement).innerText ?? null;
-                const link = (article.querySelector(".post-actions a[href*='forum/discuss.php']") as HTMLAnchorElement)?.href ?? null;
-                if (!title) return;
-                
-                if (!link) {
-                    newArticles.push(title);
-                } else {
-                    newArticles.push(`<a href="${link}">${title}</a>`);
-                }
-            });
+            const title = newsForumEl.getElementsByTagName("h2")[0];
+            if (title) {
+                title.innerText = "サイトニュース (コンパクト版)";
+            }
+            const lastlink = newsForumEl.lastElementChild?.lastElementChild as HTMLAnchorElement;
+            const subscribeButton = newsForumEl.getElementsByClassName("subscribelink")[0].children[0] as HTMLAnchorElement;
+            subscribeButton.innerText = "長いお知らせ達を読む";
+            subscribeButton.textContent = "長いお知らせ達を読む";
+            subscribeButton.href = lastlink.href;
+            const articles = newsForumEl.querySelectorAll("article.forum-post-container") as NodeListOf<HTMLDivElement>;
+            // create new element
+            const newNode = document.createElement("div");
+            newNode.innerHTML = `<h6>☆お知らせ (タイトルのみ)</h6>`;
+            if (articles.length > 0) {
+                const newArticles: string[] = [];
+                [...articles].forEach((article) => {
+                    const title = (article.getElementsByClassName("h6 font-weight-bold mb-0")[0] as HTMLHeadingElement).innerText ?? null;
+                    const link = (article.querySelector(".post-actions a[href*='forum/discuss.php']") as HTMLAnchorElement)?.href ?? null;
+                    if (!title) return;
+                    
+                    if (!link) {
+                        newArticles.push(title);
+                    } else {
+                        newArticles.push(`<a href="${link}">${title}</a>`);
+                    }
+                });
 
-            newNode.innerHTML += '<ul>' + newArticles.map((el) => `<li>${el}</li>`).join('') + '</ul>';
+                newNode.innerHTML += '<ul>' + newArticles.map((el) => `<li>${el}</li>`).join('') + '</ul>';
+            }
+            newsForumEl.lastElementChild?.remove();
+            // add element at lastElement of site-news-forum
+            newsForumEl.appendChild(newNode);
+        } catch (e) {
+            console.log("[Moodle Plus] News Feed Format is not as expected");
+            console.log(e);
         }
-        newsForumEl.lastElementChild?.remove();
-        // add element at lastElement of site-news-forum
-        newsForumEl.appendChild(newNode);
-    } catch (e) {
-        console.log("[Moodle Plus] News Feed Format is not as expected");
-        console.log(e);
     }
-}
-
 
     /**
      * Dateオブジェクトを表示用の文字列に変換します。

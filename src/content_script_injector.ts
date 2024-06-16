b79491842cc286aa0e2c6b2c9806b66de84f4565
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-06-16 21:40:17 +0900

    feat(olt): オンラインテキストの文字数カウント

--- src/content_script_injector.ts
+++ src/content_script_injector.ts
@@ -1,8 +1,26 @@
-const script = document.createElement('script');
-if (chrome) {
-    script.src = chrome.runtime.getURL('dist/content_script.js');
-} else {
-    //@ts-ignore
-    script.src = browser.runtime.getURL('dist/content_script.js');
-}
-document.body.appendChild(script);
+(() => {
+    const script = document.createElement('script');
+    let src = '';
+    
+    if (location.pathname === '/') {
+        // トップページ書き換えスクリプト
+        src = 'dist/index_rewrite.js';
+    } else if ([
+        '/mod/quiz/view.php',
+        '/mod/assign/view.php',
+        '/mod/questionnaire/view.php',
+    ].includes(location.pathname)) {
+        // オンラインテキストエディタの文字数をカウントするスクリプト
+        src = 'dist/olt_enhance.js';
+    }
+    
+    if (!src) return;
+
+    if (chrome) {
+        script.src = chrome.runtime.getURL(src);
+    } else {
+        //@ts-ignore
+        script.src = browser.runtime.getURL(src);
+    }
+    document.body.appendChild(script);    
+})();
---
+++ src/olt_enhance.ts
@@ -0,0 +1,34 @@
+(() => {
+    // オンラインテキストエディタの文字数をカウントするスクリプト
+
+    console.log("[Moodle Plus] olt enhance script loaded. Waiting for page load");
+
+    // atto editorがマウントされるまで待つ
+    window.addEventListener('load', () => {
+
+        const attoEditor = document.querySelectorAll('.editor_atto_wrap');
+        if (attoEditor.length === 0) return;
+
+        function getTextLengthInEl(el: HTMLElement): number {
+            // 異体字セレクタを考慮して文字数をカウントする
+            // ref: https://qiita.com/bon127/items/491b25e90208188dafbd#intlsegmenter
+            return [...new Intl.Segmenter('ja', { granularity: 'grapheme' }).segment(el.innerText ?? '')].length;
+        }
+
+        attoEditor.forEach((editor) => {
+            const content = editor.querySelector<HTMLElement>('[contenteditable="true"]')!; // contenteditableな要素を取得
+            const count = document.createElement('div');
+            count.style.fontSize = 'small';
+            count.style.color = '#666';
+            count.style.textAlign = 'right';
+            count.style.marginTop = '5px';
+            count.style.marginBottom = '5px';
+            count.textContent = `文字数: ${getTextLengthInEl(content)}`;
+            editor.appendChild(count);
+
+            content.addEventListener('input', () => {
+                count.textContent = `文字数: ${getTextLengthInEl(content)}`;
+            });
+        });
+    });
+})();

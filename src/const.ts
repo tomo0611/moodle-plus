7a9d4c6e623678f1264369018f25847145ec823a
tomo0611 <tomo0611@hotmail.com>
2024-12-08 21:17:30 +0900

    Released as Version 5.0

--- src/const.ts
+++ src/const.ts
@@ -6,4 +6,6 @@ export const compatibleWebsiteHostnames = [
     "sulms.shiga-u.ac.jp",
     "mdl.media.gunma-u.ac.jp",
     "cms.aitech.ac.jp",
+    // 試験対応 (Issue #25)
+    "lms-m41.mie-u.ac.jp"
 ];

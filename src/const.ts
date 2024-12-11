286e197d3146f22e9442a6a94c37c3743a3e6376
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-12-11 11:17:54 +0900

    enhance(index-rewrite): 試験対応しているMoodleについては警告を表示するように

--- src/const.ts
+++ src/const.ts
@@ -1,11 +1,21 @@
-/** 対応しているMoodleのホスト名 */
-export const compatibleWebsiteHostnames = [
+/** 動作確認が取れているMoodleのホスト名 */
+export const stableCompatibleWebsiteHostnames = [
     "lms.omu.ac.jp",
     "moodle.s.kyushu-u.ac.jp",
     "elms.u-aizu.ac.jp",
     "sulms.shiga-u.ac.jp",
     "mdl.media.gunma-u.ac.jp",
     "cms.aitech.ac.jp",
+];
+
+/** 試験対応しているMoodleのホスト名（警告が表示される） */
+export const experimentalCompatibleWebsiteHostnames = [
     // 試験対応 (Issue #25)
-    "lms-m41.mie-u.ac.jp"
+    "lms-m41.mie-u.ac.jp",
+];
+
+/** 対応しているMoodleのホスト名 */
+export const compatibleWebsiteHostnames = [
+    ...stableCompatibleWebsiteHostnames,
+    ...experimentalCompatibleWebsiteHostnames,
 ];

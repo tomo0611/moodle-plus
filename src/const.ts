e7097a1ffabe58c6b4e4dd9cd2f0d5e4f25b7ce8
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-12-11 11:18:14 +0900

    waseda moodleを試験対応校に追加

--- src/const.ts
+++ src/const.ts
@@ -12,6 +12,8 @@ export const stableCompatibleWebsiteHostnames = [
 export const experimentalCompatibleWebsiteHostnames = [
     // 試験対応 (Issue #25)
     "lms-m41.mie-u.ac.jp",
+    // 試験対応 (Issue #16)
+    "wsdmoodle.waseda.jp",
 ];
 
 /** 対応しているMoodleのホスト名 */

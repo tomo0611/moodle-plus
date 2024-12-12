c102f51e9a9796074cb5b1fdfb4c10bb1b0c4bb3
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-12-12 14:18:13 +0900

    fix(manifest): Firefoxではauthorの型が違うのに対応 (#34)

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'wxt';
+import { defineConfig, type UserManifest } from 'wxt';
 import { compatibleWebsiteHostnames } from './src/const';
 
 // See https://wxt.dev/api/config.html
@@ -12,22 +12,32 @@ export default defineConfig({
 
     // Manifest
     manifestVersion: 3,
-    manifest: {
-        name: 'Moodle Plus',
-        description: 'Moodleにちょっとした機能を追加します!',
-        author: {
-            email: 'tomo0611@hotmail.com',
-        },
-        icons: {
-            16: '/icons/icon16.png',
-            32: '/icons/icon32.png',
-            48: '/icons/icon48.png',
-            128: '/icons/icon128.png',
-            240: '/icons/icon240.png',
-        },
-        web_accessible_resources: [{
-            resources: ['index-rewrite.js'],
-            matches: compatibleWebsiteHostnames.map((hostname) => `https://${hostname}/*`),
-        }],
+    manifest: ({ browser }) => {
+        const manifest: UserManifest = {
+            name: 'Moodle Plus',
+            description: 'Moodleにちょっとした機能を追加します!',
+            icons: {
+                16: '/icons/icon16.png',
+                32: '/icons/icon32.png',
+                48: '/icons/icon48.png',
+                128: '/icons/icon128.png',
+                240: '/icons/icon240.png',
+            },
+            web_accessible_resources: [{
+                resources: ['index-rewrite.js'],
+                matches: compatibleWebsiteHostnames.map((hostname) => `https://${hostname}/*`),
+            }],
+        };
+
+        if (browser === 'firefox') {
+            // @ts-expect-error Chromeの型を使っているので異なる
+            manifest.author = 'tomo0611';
+        } else {
+            manifest.author = {
+                email: 'tomo0611@hotmail.com',
+            };
+        }
+
+        return manifest;
     },
 });
